f5357372145db8a8da1e8ce88dbc31d92ff2e60b
Mahek <114944166+mmahek@users.noreply.github.com>
2024-01-17 10:39:09 +0530

    Strings

---
+++ StringsCH3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class StringsCH3 {
+    public static void main(String[] args){
+        // String name = new String("Mahek");
+        // String is a class but can be used as a datatype
+        // String name = "Mahek";//String as a datatype
+        // System.out.println(name);
+        int a = 6;
+        float b = 24.042003f;
+         System.out.printf("The value of a is %d and b is %6.2f", a, b);
+        // System.out.format("The value of a is %d and b is %f", a, b);
+        // Format Specifiers
+        // %d -> int
+        /* %f -> float
+
+        */
+
+        // %c -> char
+        // %s -> string
+        Scanner sc = new Scanner(System.in);
+        // String a1 = sc.next();
+        // String a2 = sc.nextLine();
+        // System.out.print(a2);
+    }
+}
