761911a59fb33b0bca5b9fd13f953c91c5f2f4fe
Mahek <114944166+mmahek@users.noreply.github.com>
2024-01-18 11:39:54 +0530

    Swap New Method

---
+++ swapping.java
@@ -0,0 +1,13 @@
+public class swapping {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.println( "a = " + a);
+        System.out.println("b = " + b);
+    }
+}
