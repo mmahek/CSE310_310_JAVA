bdd08e8f9a84137a0dcf6e75b48100cdbd0af7ee
Mahek <114944166+mmahek@users.noreply.github.com>
2024-01-20 10:00:58 +0530

    Condi

--- Conditionals.java
+++ Conditionals.java
@@ -1,5 +1,35 @@
 public class Conditionals {
     public static void main(String[] args) {
-        
+         //relational operators
+         // Logical AND ( & - ampersand)    
+         /*boolean a = true;
+         boolean b = false;
+         boolean c = true;
+         if( a && b && c ){
+            System.out.println("Y");
+         }
+         else{
+            System.out.println("N");
+         }
+
+
+         // Logical OR ( | - pipe operator )
+         boolean a = true;
+         boolean b = false;
+         if( a || b ){
+            System.out.println("Y");
+         }
+         else{
+            System.out.println("N");
+         }*/
+
+          
+         // Logical Not ( ! - Inverter/Negation Operator )
+         boolean a = false;
+         boolean b = true;
+         System.out.println("Not(a) = " + !a);
+         System.out.println("Not(b) = " + !b);
+
+
     }
 }
